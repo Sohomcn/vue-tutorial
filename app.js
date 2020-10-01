40c75a65133ffef81be284cd03fc7becf003cc6f
Sohomcn <sohom.paul@capitalnumbers.com>
2020-10-02 01:15:29 +0530

    6-Event-Modifiers

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ new Vue({
 	el: '#vue-app',
 	data: {
 		age : 27,
-
-		x: 0,
-		y: 0
+		website: 'https://example.com',
 	},
 	methods: {
 		add: function(inc){
@@ -13,10 +11,8 @@ new Vue({
 		subtract: function(dec){
 			this.age -= dec;
 		},
-
-		updateXY: function(event){
-			this.x = event.offsetX;
-			this.y = event.offsetY;
+		click: function(){
+			alert('Click Triggered');
 		}
 	}
 });
