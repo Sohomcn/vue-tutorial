62cb326332c2f7b94190938673ef9f166240068b
Sohomcn <sohom.paul@capitalnumbers.com>
2020-10-02 23:36:17 +0530

    8-Two-Way-Data-Binding

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 new Vue({
 	el: '#vue-app',
 	data: {
-
+		name: '',
+		age: '',
 	},
 	methods: {
-		logName: function(){
-			console.log('Name entered');
-		},
-		logAge: function(){
-			console.log('Age entered');
-		}
+
 	}
 });
