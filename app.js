1f62f2213f95d20a43b803c5957a6e471262d102
Sohomcn <sohom.paul@capitalnumbers.com>
2020-10-03 00:34:48 +0530

    10-Dynamic-CSS-Classes

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
 new Vue({
-	el: '#vue-app',
-	data: {
-		a: 0,
-		b: 0,
-		age: 20,
-	},
-	methods: {
-		/*
-		addToA: function(){
-			console.log('addToA');
-			return this.age + this.a;
-		},
-		addToB: function(){
-			console.log('addToB');
-			return this.age + this.b;
-		}
-		*/
-	},
-	computed: {
-		addToA: function(){
-			console.log('addToA');
-			return this.age + this.a;
-		},
-		addToB: function(){
-			console.log('addToB');
-			return this.age + this.b;
-		}
-	}
+    el: '#vue-app',
+    data: {
+        available: false,
+        nearby: false
+    },
+    methods: {
+
+    },
+    computed: {
+        compClasses: function(){
+            return {
+                available: this.available,
+                nearby: this.nearby,
+            }
+        }
+    }
 });
