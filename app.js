7a42b546f7d42ac27acb8dcc57e5acf8b644d4c5
Sohomcn <sohom.paul@capitalnumbers.com>
2020-10-04 00:02:05 +0530

    12-Looping-with-v-for

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 new Vue({
     el: '#vue-app',
     data: {
-        success: false,
-        error: false
+        characters: ['sohom', 'sd', 'rupam', 'vuejs'],
+        datas: [
+            { name: 'Alex', age: 27},
+            { name: 'Robert', age: 30},
+            { name: 'John', age: 40}
+        ]
     },
     methods: {
 
     },
     computed: {
-        
+
     }
 });
